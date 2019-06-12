04345f0564b1bc2133b40726907503583b80f92c
Itorius <itorius.yt@gmail.com>
2019-06-12 20:48:28 +0200

    Init

---
+++ LayerLibrary.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ModLoader;
+
+namespace LayerLibrary
+{
+	public class LayerLibrary : Mod
+	{
+		// layer - dictionary of elements, a way to draw, update and modify the layer
+		// layer elemenent
+	}
+}
